4702f16c58d29c8e68f1fda668e252c8c0bdf881
omrucf <omrucf@aucegypt.edu>
2022-02-08 23:52:59 +0200

    completed q8

--- question 8/q8.cpp
+++ question 8/q8.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 void BinaryToDecimal(){
@@ -11,6 +12,7 @@ void BinaryToDecimal(){
     string infile="binary.txt";
     string outfile="output.txt";
     in.open(infile);
+    out.open(outfile);
     if(in.fail()){
         cout<<"failed to open file"<<endl;
     } else {
@@ -18,7 +20,10 @@ void BinaryToDecimal(){
     }
     int bin,dec=0,expo=0;
     string check;
-    in>>bin;
+    while(in>>bin){
+        check="";
+        dec=0;
+        expo=0;
     while(bin!=0){
         if(bin%10==1){
             dec+=pow(2,expo);
@@ -36,52 +41,9 @@ void BinaryToDecimal(){
     }else {
     out<<"decimal: "<<dec<<endl;
     }
-    
+    }
 }
 int main(){
     BinaryToDecimal();
    return 0;
 }
-//void BinaryToDecimal(){
-//    ifstream in;
-//    ofstream out;
-//    string infile="binary.txt";
-//    string outfile="output.txt";
-//    in.open(infile);
-//    if(in.fail()){
-//        cout<<"failed to open file"<<endl;
-//    } else {
-//        cout<<"file opened successfully"<<endl;
-//    }
-//    int size;
-//    cout<<"size?";
-//    cin>>size;
-//    int bin[size],dec[size],expo=0;
-//    string check;
-//    for(int i=0;i<size;i++){
-//    in>>bin[size];}
-//for(int i=0;i<size;i++){
-//
-//    while(bin[size]!=0){
-//        if(bin[size]%10==1){
-//            dec[size]+=pow(2,expo);
-//        } else if (bin[size]%10==0){
-//
-//        } else{
-//            check="false";
-//            break;
-//
-//        }bin[size]/=10;
-//        expo++;
-//    }
-//    if (check=="false"){
-//        cout<<"false"<<endl;
-//    }else {
-//    cout<<"decimal: "<<dec<<endl;
-//    }
-//}
-//}
-//int main(){
-//    BinaryToDecimal();
-//   return 0;
-//}
