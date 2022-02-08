33f5b635970e269f3a127a45cd04fdc50e930f03
omrucf <omrucf@aucegypt.edu>
2022-02-08 18:01:26 +0200

    Initial Commit

---
+++ assignment 0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment 0
+//
+//  Created by Omar Yossuf on 08/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
